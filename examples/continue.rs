b1766c202f5ad5221b53df18b55994bfaa8b29e0
Armin Ronacher <armin.ronacher@active-4.com>
2017-05-10 14:49:08 +0200

    Updated continuation example

--- examples/continue.rs
+++ examples/continue.rs
@@ -1,11 +1,15 @@
 extern crate dialoguer;
 
-use dialoguer::Confirmation;
+use dialoguer::{Confirmation, Input};
 
 fn main() {
     if Confirmation::new("Do you want to continue?").interact().unwrap() {
         println!("Looks like you want to continue");
     } else {
         println!("nevermind then :(");
+        return;
     }
+
+    let input = Input::new("Your name").interact().unwrap();
+    println!("Hello {}!", input);
 }
