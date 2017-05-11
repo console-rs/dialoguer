76dcac8d2b2005027ec06ec545281a98e78f32c2
Armin Ronacher <armin.ronacher@active-4.com>
2017-05-11 07:38:43 +0200

    Added checkboxes example

---
+++ examples/checkboxes.rs
@@ -0,0 +1,24 @@
+extern crate dialoguer;
+
+use dialoguer::Checkboxes;
+
+fn main() {
+    let checkboxes = &[
+        "Ice Cream",
+        "Vanilla Cupcake",
+        "Chocolate Muffin",
+        "A Pile of sweet, sweet mustard",
+    ];
+    let selections = Checkboxes::new()
+        .items(&checkboxes[..])
+        .interact().unwrap();
+
+    if selections.is_empty() {
+        println!("You did not select anything :(");
+    } else {
+        println!("You selected these things:");
+        for selection in selections {
+            println!("  {}", checkboxes[selection]);
+        }
+    }
+}
