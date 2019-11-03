f3468b2c4ca2bf3cf5309e0acbbd5f57d7b3e2aa
Benjamin Slater <benjamin.ed.slater@gmail.com>
2019-11-03 00:25:28 -0700

    removed comment

--- examples/datetime.rs
+++ examples/datetime.rs
@@ -3,8 +3,6 @@ extern crate dialoguer;
 use dialoguer::{DateTimeSelect};
 
 fn main() {
-    // There is a bug with ColorTheme for DateTimeSelect, need to look into order styles are
-    // applied.
     let datetime = DateTimeSelect::new()
         .with_prompt("Pick a time")
         .default("2019-01-01T00:00:00-08:00")
