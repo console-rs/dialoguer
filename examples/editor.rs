60f78f364b8b2ffe2cb9748715a62dbba0238fd5
Jake McGinty <me@jake.su>
2021-05-10 13:15:52 +0900

    fix example build when editor feature isn't enabled

--- examples/editor.rs
+++ examples/editor.rs
@@ -1,10 +1,15 @@
-use dialoguer::Editor;
-
+#[cfg(feature = "editor")]
 fn main() {
+    use dialoguer::Editor;
     if let Some(rv) = Editor::new().edit("Enter a commit message").unwrap() {
         println!("Your message:");
         println!("{}", rv);
     } else {
         println!("Abort!");
     }
+}
+
+#[cfg(not(feature = "editor"))]
+fn main() {
+    println!("editor feature must be enabled.");
 }
