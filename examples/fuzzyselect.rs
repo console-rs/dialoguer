a89c3dca1dbf384bbbb7adc896599bb68142a0be
Beh <b@kayuk.de>
2021-08-11 22:23:19 +0200

    Rename "position" variable in example to "selection"

--- examples/fuzzyselect.rs
+++ examples/fuzzyselect.rs
@@ -8,12 +8,12 @@ fn main() {
         "A Pile of sweet, sweet mustard",
     ];
 
-    let position = FuzzySelect::with_theme(&ColorfulTheme::default())
+    let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("Pick your flavor")
         .default(0)
         .items(&selections[..])
         .interact()
         .unwrap();
-    
-    println!("Enjoy your {}!", selections[position]);
+
+    println!("Enjoy your {}!", selections[selection]);
 }
