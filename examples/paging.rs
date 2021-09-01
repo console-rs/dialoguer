36d7b31785326fdb8b3f5a1e365090885f3e9098
Beh <github@beh.wtf>
2021-09-01 19:22:14 +0200

    Create dedicated paging example

---
+++ examples/paging.rs
@@ -0,0 +1,44 @@
+use dialoguer::{theme::ColorfulTheme, Select};
+
+fn main() {
+    let selections = &[
+        "Ice Cream",
+        "Vanilla Cupcake",
+        "Chocolate Muffin",
+        "A Pile of sweet, sweet mustard",
+        "Foo",
+        "Bar",
+        "Baz",
+        "Mustard",
+        "Cream",
+        "Banana",
+        "Chocolate",
+        "Flakes",
+        "Corn",
+        "Cake",
+        "Tarte",
+        "Clear glaze",
+        "Vanilla",
+        "Hazelnut",
+        "Flour",
+        "Sugar",
+        "Salt",
+        "Potato",
+        "French Fries",
+        "Pizza",
+        "Mousse au chocolat",
+        "Brown sugar",
+        "Blueberry",
+        "Blanc",
+    ];
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Pick your flavor")
+        .default(0)
+        .items(&selections[..])
+        .interact()
+        .unwrap();
+
+    println!("Enjoy your {}!", selections[selection]);
+
+}
