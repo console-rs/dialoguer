37221fd690914a33a5dd774cc0a384787c180303
Justin Barclay <justincbarclay@gmail.com>
2021-10-25 21:01:52 -0700

    Add max page length to sort

--- examples/sort.rs
+++ examples/sort.rs
@@ -17,4 +17,16 @@ fn main() {
     println!("  {}", list[sorted[0]]);
     println!("Your least favorite item:");
     println!("  {}", list[sorted[sorted.len() - 1]]);
+
+    let sorted = Sort::with_theme(&ColorfulTheme::default())
+        .with_prompt("Order your foods by preference")
+        .items(&list[..])
+        .max_length(2)
+        .interact()
+        .unwrap();
+
+    println!("Your favorite item:");
+    println!("  {}", list[sorted[0]]);
+    println!("Your least favorite item:");
+    println!("  {}", list[sorted[sorted.len() - 1]]);
 }
