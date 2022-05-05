537fd916c31e47fe503ba5ab476e8dfe9d0bcf5a
Mike Grunweg <grunweg@posteo.de>
2022-05-05 16:37:01 +0200

    doc: use imperative tense in password.rs.

--- src/prompts/password.rs
+++ src/prompts/password.rs
@@ -5,7 +5,7 @@ use crate::theme::{SimpleTheme, TermThemeRenderer, Theme};
 use console::Term;
 use zeroize::Zeroizing;
 
-/// Renders a password input prompt.
+/// Render a password input prompt.
 ///
 /// ## Example usage
 ///
@@ -34,20 +34,20 @@ impl Default for Password<'static> {
 }
 
 impl Password<'static> {
-    /// Creates a password input prompt.
+    /// Create a password input prompt.
     pub fn new() -> Password<'static> {
         Self::with_theme(&SimpleTheme)
     }
 }
 
 impl Password<'_> {
-    /// Sets the password input prompt.
+    /// Set the password input prompt.
     pub fn with_prompt<S: Into<String>>(&mut self, prompt: S) -> &mut Self {
         self.prompt = prompt.into();
         self
     }
 
-    /// Indicates whether to report confirmation after interaction.
+    /// Indicate whether to report confirmation after interaction.
     ///
     /// The default is to report.
     pub fn report(&mut self, val: bool) -> &mut Self {
@@ -73,7 +73,7 @@ impl Password<'_> {
         self
     }
 
-    /// Enables user interaction and returns the result.
+    /// Enable user interaction and return the result.
     ///
     /// If the user confirms the result is `true`, `false` otherwise.
     /// The dialog is rendered on stderr.
@@ -81,7 +81,7 @@ impl Password<'_> {
         self.interact_on(&Term::stderr())
     }
 
-    /// Like `interact` but allows a specific terminal to be set.
+    /// Like [`interact`](#method::interact), but allow a specific terminal to be set.
     pub fn interact_on(&self, term: &Term) -> io::Result<String> {
         let mut render = TermThemeRenderer::new(term, self.theme);
         render.set_prompts_reset_height(false);
@@ -131,7 +131,7 @@ impl Password<'_> {
 }
 
 impl<'a> Password<'a> {
-    /// Creates a password input prompt with a specific theme.
+    /// Create a password input prompt with a specific theme.
     pub fn with_theme(theme: &'a dyn Theme) -> Self {
         Self {
             prompt: "".into(),
