623acf853090492d68a4589bf7402535553fe0b0
Mike Grunweg <grunweg@posteo.de>
2022-05-05 16:57:46 +0200

    doc: document members of password.rs.

--- src/prompts/password.rs
+++ src/prompts/password.rs
@@ -20,10 +20,14 @@ use zeroize::Zeroizing;
 /// # Ok(()) } fn main() { test().unwrap(); }
 /// ```
 pub struct Password<'a> {
+    /// Message of the confirmation prompt.
     prompt: String,
+    /// Whether to print a confirmation message after selecting a password.
     report: bool,
     theme: &'a dyn Theme,
+    /// Whether an empty password is allowed.
     allow_empty_password: bool,
+    // Confirmation prompt for passwords: see [`with_confirmation`](#method::with_confirmation).
     confirmation_prompt: Option<(String, String)>,
 }
 
@@ -47,7 +51,7 @@ impl Password<'_> {
         self
     }
 
-    /// Indicate whether to report confirmation after interaction.
+    /// Indicate whether to report a confirmation after interaction.
     ///
     /// The default is to report.
     pub fn report(&mut self, val: bool) -> &mut Self {
@@ -55,7 +59,11 @@ impl Password<'_> {
         self
     }
 
-    /// Enables confirmation prompting.
+    /// Enable prompting for confirmation of the password:
+    /// if set, the user must type the same password again to confirm their choice.
+    ///
+    /// `prompt` is the prompt message for the confirmation prompt,
+    /// `mismatch_err` the error message printed upon mismatching passwords.
     pub fn with_confirmation<A, B>(&mut self, prompt: A, mismatch_err: B) -> &mut Self
     where
         A: Into<String>,
@@ -65,9 +73,9 @@ impl Password<'_> {
         self
     }
 
-    /// Allows/Disables empty password.
+    /// Allow/disallow entering an empty password.
     ///
-    /// By default this setting is set to false (i.e. password is not empty).
+    /// By default this setting is set to false (i.e. empty passwords are not allowed).
     pub fn allow_empty_password(&mut self, allow_empty_password: bool) -> &mut Self {
         self.allow_empty_password = allow_empty_password;
         self
