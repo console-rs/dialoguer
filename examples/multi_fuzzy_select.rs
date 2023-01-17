2140127d3a94ca67cef586c60f93dfbbadefb2d4
RobWalt <robwalter96@gmail.com>
2023-01-17 07:14:33 +0100

    cleanup(example): make use required feature flag in new example

--- examples/multi_fuzzy_select.rs
+++ examples/multi_fuzzy_select.rs
@@ -1,51 +1,57 @@
-use dialoguer::{theme::ColorfulTheme, MultiFuzzySelect};
-
 fn main() {
-    let multiselected = &[
-        "Ice Cream",
-        "Vanilla Cupcake",
-        "Chocolate Muffin",
-        "A Pile of sweet, sweet mustard",
-        "Carrots",
-        "Peas",
-        "Pistacio",
-        "Mustard",
-        "Cream",
-        "Banana",
-        "Chocolate",
-        "Flakes",
-        "Corn",
-        "Cake",
-        "Tarte",
-        "Cheddar",
-        "Vanilla",
-        "Hazelnut",
-        "Flour",
-        "Sugar",
-        "Salt",
-        "Potato",
-        "French Fries",
-        "Pizza",
-        "Mousse au chocolat",
-        "Brown sugar",
-        "Blueberry",
-        "Burger",
-    ];
+    #[cfg(not(feature = "fuzzy-select"))]
+    println!("feature \"fuzzy-select\" not enabled");
+    #[cfg(feature = "fuzzy-select")]
+    {
+        use dialoguer::theme::ColorfulTheme;
+        use dialoguer::MultiFuzzySelect;
 
-    let defaults = &[false, false, true, false];
-    let selections = MultiFuzzySelect::with_theme(&ColorfulTheme::default())
-        .with_prompt("Pick your food")
-        .items(&multiselected[..])
-        .defaults(&defaults[..])
-        .max_length(10)
-        .interact()
-        .unwrap();
-    if selections.is_empty() {
-        println!("You did not select anything :(");
-    } else {
-        println!("You selected these things:");
-        for selection in selections {
-            println!("  {}", multiselected[selection]);
+        let multiselected = &[
+            "Ice Cream",
+            "Vanilla Cupcake",
+            "Chocolate Muffin",
+            "A Pile of sweet, sweet mustard",
+            "Carrots",
+            "Peas",
+            "Pistacio",
+            "Mustard",
+            "Cream",
+            "Banana",
+            "Chocolate",
+            "Flakes",
+            "Corn",
+            "Cake",
+            "Tarte",
+            "Cheddar",
+            "Vanilla",
+            "Hazelnut",
+            "Flour",
+            "Sugar",
+            "Salt",
+            "Potato",
+            "French Fries",
+            "Pizza",
+            "Mousse au chocolat",
+            "Brown sugar",
+            "Blueberry",
+            "Burger",
+        ];
+
+        let defaults = &[false, false, true, false];
+        let selections = MultiFuzzySelect::with_theme(&ColorfulTheme::default())
+            .with_prompt("Pick your food")
+            .items(&multiselected[..])
+            .defaults(&defaults[..])
+            .max_length(10)
+            .interact()
+            .unwrap();
+        if selections.is_empty() {
+            println!("You did not select anything :(");
+        } else {
+            println!("You selected these things:");
+            for selection in selections {
+                println!("\t{}", multiselected[selection]);
+            }
         }
     }
 }
