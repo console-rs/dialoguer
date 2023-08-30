ed5ddc26d5a6c286780c0bb3620f7822da6ee74f
Alexander Sergeev <etc9053@gmail.com>
2023-08-30 14:04:27 +0300

    Change chars for `MultiSelect` items

--- src/theme/colorful.rs
+++ src/theme/colorful.rs
@@ -71,8 +71,8 @@ impl Default for ColorfulTheme {
             inactive_item_style: Style::new().for_stderr(),
             active_item_prefix: style("❯".to_string()).for_stderr().green(),
             inactive_item_prefix: style(" ".to_string()).for_stderr(),
-            checked_item_prefix: style("✔".to_string()).for_stderr().green(),
-            unchecked_item_prefix: style("✔".to_string()).for_stderr().black(),
+            checked_item_prefix: style("✔️".to_string()).for_stderr().blue(),
+            unchecked_item_prefix: style("❌".to_string()).for_stderr().red(),
             picked_item_prefix: style("❯".to_string()).for_stderr().green(),
             unpicked_item_prefix: style(" ".to_string()).for_stderr(),
             #[cfg(feature = "fuzzy-select")]
