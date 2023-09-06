0a3a888a6258188fa4366c1727457cb8d899a58f
Alexander Sergeev <etc9053@gmail.com>
2023-09-06 17:14:29 +0300

    Fix password example too

--- src/prompts/password.rs
+++ src/prompts/password.rs
@@ -159,7 +159,7 @@ impl<'a> Password<'a> {
     ///     let password: String = Password::new()
     ///         .with_prompt("Enter password")
     ///         .validate_with(|input: &String| -> Result<(), &str> {
-    ///             if input.len() > 8 {
+    ///             if input.chars().count() > 8 {
     ///                 Ok(())
     ///             } else {
     ///                 Err("Password must be longer than 8")
